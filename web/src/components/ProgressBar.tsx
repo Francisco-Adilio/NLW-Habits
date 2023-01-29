e5f2158c4bff8766afd294699e3c71a472719b12
Francisco Adilio Lessa <franciscoadiliolessa@gmail.com>
2023-01-29 12:42:13 -0300

    Sync habits progress and add transitions

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -8,9 +8,9 @@ export function ProgressBar(props: IProgessBar) {
   return (
     <Progress.Root className="h-3 rounded-xl bg-zinc-700 w-full mt-4" value={props.progress}>
       <Progress.Indicator
-        className="h-3 rounded-xl bg-violet-600"
+        className="h-3 rounded-xl bg-violet-600 transition-all"
         style={{
-          width: `${props.progress}%`
+          width: `${props.progress < 100 ? props.progress : 100}%`
         }}
       />
     </Progress.Root>
